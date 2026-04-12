7f7d2faeafeeaf3ec86fbbe6f70bc4c328f7f821
heshanrusiru792 <heshanrusiru792@gmail.com>
2026-04-12 13:02:16 +0530

    Update post.js

--- post.js
+++ post.js
@@ -29,6 +29,12 @@
     "post_name": "Internal Panel Updated",
    "post_image": "https://images-ext-1.discordapp.net/external/tJ85k8axOGejy2xZ_DGpbNdQj0QPkYhbJM4rXJCfLro/https/i.ibb.co/d4hhfYfm/2026-04-1111-10-59-ezgif-com-video-to-gif-converter-1.gif",
     "post": "speed hack risky"
+  },
+    {
+    "category": "Panel Updates",
+    "post_name": "Premium/Plus Panel Updated",
+   "post_image": "https://images-ext-1.discordapp.net/external/tJ85k8axOGejy2xZ_DGpbNdQj0QPkYhbJM4rXJCfLro/https/i.ibb.co/d4hhfYfm/2026-04-1111-10-59-ezgif-com-video-to-gif-converter-1.gif",
+    "post": "aimbot and sniper functions"
   }
   
 ]
