717eaf740f587ee029603949e5e54e28836f30e3
heshanrusiru792 <heshanrusiru792@gmail.com>
2026-04-22 23:59:36 +0530

    Update post.js

--- post.js
+++ post.js
@@ -28,7 +28,13 @@
     "category": "Panel Updates",
     "post_name": "Internal Panel Updated",
    "post_image": "https://images-ext-1.discordapp.net/external/rRMA9wqFR2oY9r928M5M5BmZCv7KY_XWEaHjgLQV8m0/https/i.ibb.co/VWrxwcdm/standard.gif?width=400&height=160",
-    "post": "ESP Bug Fix , Silent aim Power up , aim froce updated"
+    "post": "ESP Bug Fix , Silent aim Power up , aim froce updated , anticheat added"
+  },
+    {
+    "category": "Panel Updates",
+    "post_name": "Premium Panel Updated",
+   "post_image": "https://images-ext-1.discordapp.net/external/rRMA9wqFR2oY9r928M5M5BmZCv7KY_XWEaHjgLQV8m0/https/i.ibb.co/VWrxwcdm/standard.gif?width=400&height=160",
+    "post": "bypass emulater , guest reset, no recoil, speed hck functions updated"
   }
   
 ]
