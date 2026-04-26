f941e90fdc765fee11dbdd68b1863ad15c57c603
heshanrusiru792 <heshanrusiru792@gmail.com>
2026-04-26 12:37:46 +0530

    Update post data structure in post.js

--- post.js
+++ post.js
@@ -38,9 +38,9 @@
   },
     {
     "category": "Panel Updates",
-    "post_name": "Alert ⚠️",
+    "post_name": "Premium Panel Updated",
    "post_image": "https://images-ext-1.discordapp.net/external/rRMA9wqFR2oY9r928M5M5BmZCv7KY_XWEaHjgLQV8m0/https/i.ibb.co/VWrxwcdm/standard.gif?width=400&height=160",
-    "post": "Today Don't use sniper functions!!"
+    "post": "Auto save user pass , sniper scope updated"
   }
   
 ]
