1773deeb88337a5978ac233f8754105039b06455
heshanrusiru792 <heshanrusiru792@gmail.com>
2026-05-03 11:32:54 +0530

    Update post.js

--- post.js
+++ post.js
@@ -47,6 +47,12 @@
     "post_name": "Premium/plus panel updated",
    "post_image": "https://images-ext-1.discordapp.net/external/rRMA9wqFR2oY9r928M5M5BmZCv7KY_XWEaHjgLQV8m0/https/i.ibb.co/VWrxwcdm/standard.gif?width=400&height=160",
     "post": "extra functions and aimbot functions updated"
+  },
+    {
+    "category": "Panel Updates",
+    "post_name": "Download panel !!",
+   "post_image": "https://images-ext-1.discordapp.net/external/rRMA9wqFR2oY9r928M5M5BmZCv7KY_XWEaHjgLQV8m0/https/i.ibb.co/VWrxwcdm/standard.gif?width=400&height=160",
+    "post": "Updated panel you must download new exe from this website \n https://www.heshanmodz.com/freefire/"
   }
   
   
