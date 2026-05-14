4e2a2b2c119c50ef9eabd527cc2d6baec868ab81
heshanrusiru792 <heshanrusiru792@gmail.com>
2026-05-14 11:33:02 +0530

    Update post.js

--- post.js
+++ post.js
@@ -41,6 +41,12 @@
     "post_name": "Internal Updated",
    "post_image": "https://images-ext-1.discordapp.net/external/rRMA9wqFR2oY9r928M5M5BmZCv7KY_XWEaHjgLQV8m0/https/i.ibb.co/VWrxwcdm/standard.gif?width=400&height=160",
     "post": "Aim silent , overlay , aimbot ai , fly run updated use your own risk."
+  },
+    {
+    "category": "Panel Updates",
+    "post_name": "Alert ⚠️",
+   "post_image": "https://images-ext-1.discordapp.net/external/rRMA9wqFR2oY9r928M5M5BmZCv7KY_XWEaHjgLQV8m0/https/i.ibb.co/VWrxwcdm/standard.gif?width=400&height=160",
+    "post": "Don't use today in your main id (2026.05.14)"
   }
   
 ]
