b99f9ea5498d09de273123f9d29ecdc87833cf38
heshanrusiru792 <heshanrusiru792@gmail.com>
2026-05-14 17:07:19 +0530

    Update post.js

--- post.js
+++ post.js
@@ -46,7 +46,7 @@
     "category": "Panel Updates",
     "post_name": "Alert ⚠️",
    "post_image": "https://images-ext-1.discordapp.net/external/rRMA9wqFR2oY9r928M5M5BmZCv7KY_XWEaHjgLQV8m0/https/i.ibb.co/VWrxwcdm/standard.gif?width=400&height=160",
-    "post": "Don't use today in your main id (2026.05.14)"
+    "post": "Panel safe now, lag function also safe"
   }
   
 ]
