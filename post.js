4c9cf266cf4252a8106ccf2ff6bcb72163252d21
heshanrusiru792 <heshanrusiru792@gmail.com>
2026-05-17 22:03:55 +0530

    Update post.js

--- post.js
+++ post.js
@@ -32,15 +32,9 @@
   },
     {
     "category": "Panel Updates",
-    "post_name": "Internal Updated",
+    "post_name": "Panel Updated",
    "post_image": "https://images-ext-1.discordapp.net/external/rRMA9wqFR2oY9r928M5M5BmZCv7KY_XWEaHjgLQV8m0/https/i.ibb.co/VWrxwcdm/standard.gif?width=400&height=160",
-    "post": "Aim silent , overlay , aimbot ai , fly run updated use your own risk."
-  },
-    {
-    "category": "Panel Updates",
-    "post_name": "Alert ⚠️",
-   "post_image": "https://images-ext-1.discordapp.net/external/rRMA9wqFR2oY9r928M5M5BmZCv7KY_XWEaHjgLQV8m0/https/i.ibb.co/VWrxwcdm/standard.gif?width=400&height=160",
-    "post": "Panel safe now, lag function also safe"
+    "post": "Updated panel and functions."
   }
   
 ]
