33b9358f8c1a6bac33c8359dd28c7b9e20378f0d
heshanrusiru792 <heshanrusiru792@gmail.com>
2026-05-31 10:14:22 +0530

    Update post.js

--- post.js
+++ post.js
@@ -26,9 +26,9 @@
   },
     {
     "category": "Panel Updates",
-    "post_name": "Alert",
+    "post_name": "External /Internal Updated",
    "post_image": "https://images-ext-1.discordapp.net/external/rRMA9wqFR2oY9r928M5M5BmZCv7KY_XWEaHjgLQV8m0/https/i.ibb.co/VWrxwcdm/standard.gif?width=400&height=160",
-    "post": "Please note that today from 12:30 PM to 4:30 PM, do not use any panels connected to the main ID due to scheduled maintenance and monitoring activities.Kindly use alternative accounts/panels during this time period to avoid interruptions."
+    "post": "External vsk scope pc version for working now, Internal panel now pc version for working now."
   }
 
   
