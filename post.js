ad4fff5ffb34b33cbe778178c1840c72ce434a71
heshanrusiru792 <heshanrusiru792@gmail.com>
2026-06-03 21:09:07 +0530

    Update post.js

--- post.js
+++ post.js
@@ -26,9 +26,9 @@
   },
     {
     "category": "Panel Updates",
-    "post_name": "Alert ⚠️",
+    "post_name": "external updated",
    "post_image": "https://images-ext-1.discordapp.net/external/rRMA9wqFR2oY9r928M5M5BmZCv7KY_XWEaHjgLQV8m0/https/i.ibb.co/VWrxwcdm/standard.gif?width=400&height=160",
-    "post": "Today (2026.6.3) don't use main id 11.00am to 4.00pm, today high risk day."
+    "post": "Lag functions updated success."
   }
 
   
