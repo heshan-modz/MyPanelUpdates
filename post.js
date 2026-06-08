838e92d1ab46b50dd3c750e39cdc775cac816855
heshanrusiru792 <heshanrusiru792@gmail.com>
2026-06-08 07:34:51 +0530

    Update post.js

--- post.js
+++ post.js
@@ -29,6 +29,12 @@
     "post_name": "Alert ⚠️",
    "post_image": "https://images-ext-1.discordapp.net/external/rRMA9wqFR2oY9r928M5M5BmZCv7KY_XWEaHjgLQV8m0/https/i.ibb.co/VWrxwcdm/standard.gif?width=400&height=160",
     "post": "Everymode kill limit is 9"
+  },
+    {
+    "category": "Panel Updates",
+    "post_name": "Alert ⚠️",
+   "post_image": "https://images-ext-1.discordapp.net/external/rRMA9wqFR2oY9r928M5M5BmZCv7KY_XWEaHjgLQV8m0/https/i.ibb.co/VWrxwcdm/standard.gif?width=400&height=160",
+    "post": "Premium/Plus Panel Safe now"
   }
 
 
