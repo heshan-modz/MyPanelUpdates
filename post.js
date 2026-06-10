2e494f783f88a9a9b46cf895e754132b59c40f21
heshanrusiru792 <heshanrusiru792@gmail.com>
2026-06-10 18:07:39 +0530

    Update post.js

--- post.js
+++ post.js
@@ -30,23 +30,17 @@
    "post_image": "https://images-ext-1.discordapp.net/external/rRMA9wqFR2oY9r928M5M5BmZCv7KY_XWEaHjgLQV8m0/https/i.ibb.co/VWrxwcdm/standard.gif?width=400&height=160",
     "post": "Everymode kill limit is 9"
   },
-    {
-    "category": "Panel Updates",
-    "post_name": "Alert ⚠️",
-   "post_image": "https://images-ext-1.discordapp.net/external/rRMA9wqFR2oY9r928M5M5BmZCv7KY_XWEaHjgLQV8m0/https/i.ibb.co/VWrxwcdm/standard.gif?width=400&height=160",
-    "post": "Premium/Plus Panel Safe now"
-  },
   {
     "category": "FreeFire News",
     "post_name": "FreeFire OB54 Update",
     "post_image": "https://images-ext-1.discordapp.net/external/BtbRUqwffrwIjfNB58tIojU9_p7PaMC7AXRrM-WR34Y/https/i.ibb.co/LzmtvVjp/image-2026-06-08-164026765.png?format=webp&quality=lossless",
-    "post": "remaining 15 days"
+    "post": "remaining 13 days"
   },
-  {
-    "category": "FreeFire News",
-    "post_name": "How to Recover FootBall kick Hks ??",
-    "post_image": "https://images-ext-1.discordapp.net/external/rRMA9wqFR2oY9r928M5M5BmZCv7KY_XWEaHjgLQV8m0/https/i.ibb.co/VWrxwcdm/standard.gif?width=400&height=160",
-    "post": "oylta puluvn football kick eken berenna oylth football ek dgnna 4 denma gihin football kick ek use krna kenage gava gihin fire krnn football ek kadenakan ek kadunama recover venna puluvn"
+    {
+    "category": "Panel Updates",
+    "post_name": "Premium panel updated",
+   "post_image": "https://images-ext-1.discordapp.net/external/rRMA9wqFR2oY9r928M5M5BmZCv7KY_XWEaHjgLQV8m0/https/i.ibb.co/VWrxwcdm/standard.gif?width=400&height=160",
+    "post": "added menu v2 2d kit fixed,ghost lag updated"
   }
 
 
