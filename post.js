a2f04be679b15cf5fef66792d90579f8b627cffe
heshanrusiru792 <heshanrusiru792@gmail.com>
2026-06-17 08:24:21 +0530

    Update post.js

--- post.js
+++ post.js
@@ -34,13 +34,7 @@
     "category": "FreeFire News",
     "post_name": "FreeFire OB54 Update",
     "post_image": "https://images-ext-1.discordapp.net/external/BtbRUqwffrwIjfNB58tIojU9_p7PaMC7AXRrM-WR34Y/https/i.ibb.co/LzmtvVjp/image-2026-06-08-164026765.png?format=webp&quality=lossless",
-    "post": "remaining 9 days"
-  },
-    {
-    "category": "Panel Updates",
-    "post_name": "Premium / plus updated",
-   "post_image": "https://images-ext-1.discordapp.net/external/rRMA9wqFR2oY9r928M5M5BmZCv7KY_XWEaHjgLQV8m0/https/i.ibb.co/VWrxwcdm/standard.gif?width=400&height=160",
-    "post": "Pluse : 2d kit chams menu added, Premium : lag functions updated "
+    "post": "remaining 5 days"
   }
 
 
