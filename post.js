de691707b2638729acf3943eb72d70238ea3737a
heshanrusiru792 <heshanrusiru792@gmail.com>
2026-06-24 00:10:34 +0530

    Update post.js

--- post.js
+++ post.js
@@ -2,9 +2,9 @@
 
   {
     "category": "FreeFire News",
-    "post_name": "FreeFire OB53 Update",
-    "post_image": "https://dl.dir.freefiremobile.com/common/web_event/official2.ff.garena.all/20264/c57c0a509dd55e527fd73a89d3aae479.jpeg",
-    "post": "Date of Update: Insert Date – 8th April 2026\nThis is to inform all FreeFire players that the highly anticipated OB53\nUpdate available for download after maintenance"
+    "post_name": "FreeFire OB54 Update",
+    "post_image": "https://images-ext-1.discordapp.net/external/BtbRUqwffrwIjfNB58tIojU9_p7PaMC7AXRrM-WR34Y/https/i.ibb.co/LzmtvVjp/image-2026-06-08-164026765.png?format=webp&quality=lossless",
+    "post": "Date of Update: Insert Date – 24th June 2026\nThis is to inform all FreeFire players that the highly anticipated OB54\nUpdate available for download after maintenance"
   },
     {
     "category": "Panel Updates",
@@ -29,14 +29,7 @@
     "post_name": "Alert ⚠️",
    "post_image": "https://images-ext-1.discordapp.net/external/rRMA9wqFR2oY9r928M5M5BmZCv7KY_XWEaHjgLQV8m0/https/i.ibb.co/VWrxwcdm/standard.gif?width=400&height=160",
     "post": "Everymode kill limit is 9"
-  },
-  {
-    "category": "FreeFire News",
-    "post_name": "FreeFire OB54 Update",
-    "post_image": "https://images-ext-1.discordapp.net/external/BtbRUqwffrwIjfNB58tIojU9_p7PaMC7AXRrM-WR34Y/https/i.ibb.co/LzmtvVjp/image-2026-06-08-164026765.png?format=webp&quality=lossless",
-    "post": "tommorow"
   }
-
 
   
 ]
