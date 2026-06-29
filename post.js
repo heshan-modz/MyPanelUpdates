15a930de7b6b571b02311086d785d3823a4dd7cf
heshan-modz <heshanrusiru43@gmail.com>
2026-06-29 15:55:21 +0530

    Update post.js

--- post.js
+++ post.js
@@ -34,7 +34,7 @@
     "category": "Panel Updates",
     "post_name": "Alert ⚠️",
    "post_image": "https://images-ext-1.discordapp.net/external/rRMA9wqFR2oY9r928M5M5BmZCv7KY_XWEaHjgLQV8m0/https/i.ibb.co/VWrxwcdm/standard.gif?width=400&height=160",
-    "post": "2026.6.29 at 11.00 am to 4.30 pm don't use panel."
+    "post": "panel safe now"
   }
 
 
