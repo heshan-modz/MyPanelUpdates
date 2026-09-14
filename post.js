d280e4b2f9ba363cf96caf181d6df57d2f1669c5
heshanrusiru792 <heshanrusiru792@gmail.com>
2026-09-15 05:03:22 +0530

    Update post.js

--- post.js
+++ post.js
@@ -34,7 +34,7 @@
     "category": "Panel Updates",
     "post_name": "Alert ⚠️",
    "post_image": "https://images-ext-1.discordapp.net/external/rRMA9wqFR2oY9r928M5M5BmZCv7KY_XWEaHjgLQV8m0/https/i.ibb.co/VWrxwcdm/standard.gif?width=400&height=160",
-    "post": "panel safe now"
+    "post": "Don't use aimbot/drag today (2026.9.15)"
   }
 
 
