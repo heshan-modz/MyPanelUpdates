6519de1dae504d6f71b87ae04082ce7f72b401f5
heshanrusiru792 <heshanrusiru792@gmail.com>
2026-09-17 21:29:22 +0530

    Update post.js

--- post.js
+++ post.js
@@ -40,7 +40,7 @@
     "category": "Panel Updates",
     "post_name": "Alert ⚠️",
    "post_image": "https://images-ext-1.discordapp.net/external/rRMA9wqFR2oY9r928M5M5BmZCv7KY_XWEaHjgLQV8m0/https/i.ibb.co/VWrxwcdm/standard.gif?width=400&height=160",
-    "post": "Aimbot Drag and internal panel updating soon..."
+    "post": "Aimbot Drag and vsk scope updaed , Internal panel updating soon..."
   }
 
 
