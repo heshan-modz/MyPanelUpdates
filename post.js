a205ee430e9ce0c115b131403bcdfd389e9d9703
heshanrusiru792 <heshanrusiru792@gmail.com>
2026-09-18 22:55:06 +0530

    Update post.js

--- post.js
+++ post.js
@@ -40,8 +40,15 @@
     "category": "Panel Updates",
     "post_name": "Alert ⚠️",
    "post_image": "https://images-ext-1.discordapp.net/external/rRMA9wqFR2oY9r928M5M5BmZCv7KY_XWEaHjgLQV8m0/https/i.ibb.co/VWrxwcdm/standard.gif?width=400&height=160",
-    "post": "Aimbot Drag and vsk scope updaed , Internal panel updating soon..."
+    "post": "Internal Users FreeFire Apk : https://www.mediafire.com/file/cu08kr36ty0yqey/FREEFIRE_V7A_x_HeshaN_ModZ.xapk/file"
+  },
+    {
+    "category": "Panel Updates",
+    "post_name": "Alert ⚠️",
+   "post_image": "https://images-ext-1.discordapp.net/external/rRMA9wqFR2oY9r928M5M5BmZCv7KY_XWEaHjgLQV8m0/https/i.ibb.co/VWrxwcdm/standard.gif?width=400&height=160",
+    "post": "For internal users aimbot visible dont use main id."
   }
+
 
 
 
