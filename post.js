3da462d82b35bbe3a2b9ac94a6e77bf70c654d24
heshanrusiru792 <heshanrusiru792@gmail.com>
2026-09-25 03:41:05 +0530

    Update post.js

--- post.js
+++ post.js
@@ -46,7 +46,7 @@
     "category": "Panel Updates",
     "post_name": "Alert ⚠️",
    "post_image": "https://images-ext-1.discordapp.net/external/rRMA9wqFR2oY9r928M5M5BmZCv7KY_XWEaHjgLQV8m0/https/i.ibb.co/VWrxwcdm/standard.gif?width=400&height=160",
-    "post": "Internal panel don't use main id"
+    "post": "Internal panel safe now"
   },
     {
     "category": "Panel Updates",
